dff52a415a5b6361457bef588f234d368d1df593
artamonova-anastasiia <artamonova.anastasiia@e-u.edu.ua>
2025-11-06 13:57:17 +0200

    Praktichna6

---
+++ script.js
@@ -0,0 +1,10 @@
+// TODO: Implement the addTask function
+function addTask() {
+    // 1. Get the input value from the text box
+    // 2. Validate the input (not empty)
+    // 3. Create a new list item (li) with task text
+    // 4. Add complete and remove buttons to each task
+    // 5. Add the new item to the task list (ul)
+    // 6. Clear the input field
+    // Your code goes here
+}
